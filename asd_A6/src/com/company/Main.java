bbdd6d2430cdb16e17a67dd54057d9c003f4d39e
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-11-07 10:33:30 +0100

    get rid of unnecessary logic

--- asd_A6/src/com/company/Main.java
+++ asd_A6/src/com/company/Main.java
@@ -8,47 +8,23 @@ public class Main {
 	    int min = 0;
         int max = 1000;
         int a = getRandomNumber(min, max);
-        boolean isGuessedInFirstAttempt = false;
 
         System.out.println(a);
 
-        int firstAttempt = max/2;
-
-        System.out.println("Czy to liczba " + firstAttempt + "?");
-
-        if(a == firstAttempt){
-            System.out.println("TAK");
-            isGuessedInFirstAttempt=false;
-        }
-        if(firstAttempt > a){
-            System.out.println("ZA DUŻA");
-            max=firstAttempt;
-        }else if(firstAttempt < a){
-            System.out.println("ZA MALA");
-            min = firstAttempt;
-        }
-
-        if(!isGuessedInFirstAttempt){
-            for(int i = 0; i < 2; i--) {
-                if (i != 0) {
-                    System.out.println("Czy to liczba " + (int)((min+max)/2) + "?");
-
-                    if (a == (int)((min+max)/2)) {
-                        System.out.println("TAK");
-                        break;
-                    }
-
-                    if((int)((min+max)/2) > a){
-                        System.out.println("ZA DUŻA");
-                        max = (int)((min+max)/2);
-                    }else if((int)((min+max)/2) < a){
-                        System.out.println("ZA MALA");
-                        min = (int)((min+max)/2);
-                    }
-
-                    TimeUnit.SECONDS.sleep(1);
-                }
+        while(true){
+            System.out.println("Czy to liczba " + (int)((min+max)/2) + "?");
+            if(a == (int)((min+max)/2)){
+                System.out.println("TAK");
+                break;
             }
+            if((int)((min+max)/2) > a){
+                System.out.println("ZA DUŻA");
+                max = (int)((min+max)/2);
+            }else if((int)((min+max)/2) < a){
+                System.out.println("ZA MALA");
+                min = (int)((min+max)/2);
+            }
+            TimeUnit.SECONDS.sleep(1);
         }
     }
 
