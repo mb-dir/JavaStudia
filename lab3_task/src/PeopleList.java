2d3498dfc1a55fc903671e92f34bad11612b666d
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-11-14 17:11:21 +0100

    Show user data when chosen

--- lab3_task/src/PeopleList.java
+++ lab3_task/src/PeopleList.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -28,18 +31,51 @@ public class PeopleList extends JFrame {
 
         ArrayList<Person> listOfPeople = new ArrayList<Person>();
 
-        for(int i = 0; i < 5; i++){
-            Person p = new Person("jan", "paweł","2137", "watykan", new Date("February 09, 2004"));
+        for(int i = 0; i <= 5; i++){
+            Person p = new Person("jan" + i, "paweł" + i,"2137", "watykan", new Date("February 09, 2004"));
             listOfPeople.add(p);
         }
 
-        String peopleData[] = new String[5];
+        String peopleData[] = new String[6];
+        peopleData[0]="";
 
-        for(int i = 0; i < 5; i++){
-            String personData = listOfPeople.get(i).name + " " + listOfPeople.get(i).surname + (i+1);
+        for(int i = 1; i <= 5; i++){
+            String personData = listOfPeople.get(i).name + " " + listOfPeople.get(i).surname;
             peopleData[i] = personData;
         }
-        list1.setModel(new DefaultComboBoxModel(peopleData));
+
+        comboBox1.setModel(new DefaultComboBoxModel(peopleData));
+
+
+        comboBox1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String data = ""+comboBox1.getItemAt(comboBox1.getSelectedIndex());
+                String name = data.split(" ")[0];
+
+                Person selectedPerson = null;
+
+                for(Person p : listOfPeople) {
+                    if (p.name.equals(name)) {
+                        selectedPerson = p;
+                    }
+                }
+
+                if(selectedPerson != null){
+                    nameInput.setText(selectedPerson.name);
+                    surnameInput.setText(selectedPerson.surname);
+                    phoneInput.setText(selectedPerson.phoneNumber);
+                    addressInput.setText(selectedPerson.address);
+                    DoFInput.setText(selectedPerson.dateOfBirth.toString());
+                }else{
+                    nameInput.setText("");
+                    surnameInput.setText("");
+                    phoneInput.setText("");
+                    addressInput.setText("");
+                    DoFInput.setText("");
+                }
+            }
+        });
 
     }
 }
