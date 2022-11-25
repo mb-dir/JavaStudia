e7eab08295c92ecf48f2248bd0b2d4d4d0d5f443
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-11-25 19:17:53 +0100

    make recursion solution

--- asd_A6/src/com/company/Main.java
+++ asd_A6/src/com/company/Main.java
@@ -7,24 +7,25 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 	    int min = 0;
         int max = 1000;
-        int a = getRandomNumber(min, max);
+        int a = 125;
 
         System.out.println(a);
+        recursion(min, 500, max, a);
 
-        while(true){
-            System.out.println("Czy to liczba " + (int)((min+max)/2) + "?");
-            if(a == (int)((min+max)/2)){
-                System.out.println("TAK");
-                break;
-            }
-            if((int)((min+max)/2) > a){
-                System.out.println("ZA DUŻA");
-                max = (int)((min+max)/2);
-            }else if((int)((min+max)/2) < a){
-                System.out.println("ZA MALA");
-                min = (int)((min+max)/2);
-            }
-            TimeUnit.SECONDS.sleep(1);
+    }
+
+    public static int recursion(int left, int p, int right, int a) throws InterruptedException {
+        TimeUnit.SECONDS.sleep(1);
+        if(p == a){
+            System.out.println(p + " znaleziono");
+            return 0;
+        }
+        if(p < a){
+            System.out.println(p + " za mala");
+            return recursion(p, (int)(p+right+1)/2, right, a);
+        }else{
+            System.out.println(p + " za duza");
+            return recursion(left, (int)(p+left-1)/2, p, a);
         }
     }
 
